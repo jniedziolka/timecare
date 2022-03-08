2b9df6651bb1181cdc4b41812094917554a3feb8
Jakub Niedziółka <jakub.niedziolka@o2.pl>
2022-03-08 22:32:12 +0100

    Added possibility of storing a new project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreProjectRequest;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
+    public function store(StoreProjectRequest $request)
+    {
+        $validated = $request->validated();
 
+        return Project::create([
+            'name' => $validated['name']
+        ]);
+    }
 }
